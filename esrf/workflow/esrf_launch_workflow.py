849585862a2efc225c8a565b4d0aba726034402f
Olof Svensson <svensson@scisoft10.esrf.fr>
2022-07-18 17:20:15 +0200

    Fix of typo

--- esrf/workflow/esrf_launch_workflow.py
+++ esrf/workflow/esrf_launch_workflow.py
@@ -367,7 +367,7 @@ else:
             db = 1
         elif proposal == "mx2112":
             # Use valid data base
-            print("ISPyB production data base used")
+            print("ISPyB valid data base used")
             db = 1
         else:
             # Use productiond data base

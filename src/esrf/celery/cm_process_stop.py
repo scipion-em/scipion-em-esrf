955ca48178a91034e7f309222d035352ebe76e34
Olof Svensson <svensson@esrf.fr>
2023-06-02 11:31:07 +0200

    Bug fix for wrong import

--- src/esrf/celery/cm_process_stop.py
+++ src/esrf/celery/cm_process_stop.py
@@ -1,11 +1,10 @@
-import pprint
 import sys
 import time
 
 import celery
 
 
-from esrf.sp.cm_process_status import print_worker_status
+from esrf.celery.cm_worker_status import print_worker_status
 
 app = celery.Celery()
 app.config_from_object("esrf.celery.cm_config")
@@ -24,6 +23,7 @@ else:
     worker_to_abort = list_processing[0]
 worker_value = active_workers[worker_to_abort]
 for dict_worker in worker_value:
+    config_dict = None
     celery_id = dict_worker["id"]
     list_args = dict_worker["args"]
     for dict_args in list_args:
@@ -31,20 +31,23 @@ for dict_worker in worker_value:
             if arg_key == "dataDirectory":
                 config_dict = dict_args
                 break
-    directory = config_dict["dataDirectory"]
-    are_sure = input(f"Are you sure you want to abort job in {directory}? ")
-    if are_sure.lower() != "yes":
-        print("Not aborting worker.")
+    if config_dict is None:
+        raise RuntimeError(f"Cannot find 'dataDirectory' for celery_id {celery_id}")
     else:
-        print("Revoking celery job...")
-        celery.current_app.control.revoke(celery_id, terminate=True)
-        # Sleep a couple of seconds
-        time.sleep(10)
-        # Kill the remaining processes
-        print("Killing remaining processes.")
-        future = app.send_task(
-            "esrf.sp.cm_process_worker.kill_workflow",
-            args=(config_dict,),
-            queue=worker_to_abort,
-        )
-        print(f"Job with celery id {celery_id} aborted.")
+        directory = config_dict["dataDirectory"]
+        are_sure = input(f"Are you sure you want to abort job in {directory}? ")
+        if are_sure.lower() != "yes":
+            print("Not aborting worker.")
+        else:
+            print("Revoking celery job...")
+            celery.current_app.control.revoke(celery_id, terminate=True)
+            # Sleep a couple of seconds
+            time.sleep(10)
+            # Kill the remaining processes
+            print("Killing remaining processes.")
+            future = app.send_task(
+                "esrf.sp.cm_process_worker.kill_workflow",
+                args=(config_dict,),
+                queue=worker_to_abort,
+            )
+            print(f"Job with celery id {celery_id} aborted.")
